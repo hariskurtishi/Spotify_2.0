ce3310e1e31f4dd1c0c0c4546c4492736edc5581
hariskurtishi <Hariskay95@gmail.com>
2022-11-02 00:50:37 +0100

    Added song details component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { FreeMode } from 'swiper'
+import { FreeMode } from 'swiper'   
    
 import PlayPause from './PlayPause'
 import { playPause, setActiveSong } from '../redux/features/playerSlice'
@@ -11,9 +11,29 @@ import { useGetTopChartsQuery } from '../redux/services/shazamCore'
 import 'swiper/css'
 import 'swiper/css/free-mode'
 
-const TopChartCard = ({song, i}) => (
+const TopChartCard = ({song, i, isPlaying, activeSong, handlePlayClick, handlePauseClick}) => (
   <div className='w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2'>
-    {song.title}
+    <h3 className='font-bold text-base text-white mr-3'> {i + 1}.</h3>
+    <div className='flex-1 flex flex-row justify-between items-center'>
+  <img className='w-20 h-20 rounded-lg' src={song?.images?.coverart} alt={song?.title} />
+  <div className='flex-1 flex flex-col justify-center mx-3'>
+<Link to={`/songs/${song.key}`}>
+  <p className='text-xl font-bold text-white'>{song?.title}</p>
+</Link>
+<Link to={`/artists/${song?.artists[0].adamid}`}>
+  <p className='text-base text-gray-300 mt-1'>{song?.subtitle}</p>
+</Link>
+  </div>
+    </div>
+    <PlayPause 
+    isPlaying={isPlaying}
+    activeSong={activeSong}
+    song={song}
+    handlePause={handlePauseClick}
+    handlePlay={handlePlayClick}
+
+    
+    />
   </div>
 )
 
@@ -35,7 +55,7 @@ const TopPlay = () =>  {
 
 }
 
-const handlePlayClick = () => {
+const handlePlayClick = (song, i) => {
   dispatch(setActiveSong({ song, data, i }))
   dispatch(playPause(true))
 }
@@ -56,6 +76,10 @@ return (
     key={song.key}
     song={song}
     i={i}
+    isPlaying={isPlaying}
+    activeSong={activeSong}
+    handlePauseClick={handlePauseClick}
+    handlePlayClick={() => handlePlayClick(song, i)}
     />
   ))}
   </div>
@@ -87,10 +111,8 @@ className="mt-4">
         <img src={song?.images.background} alt="name"
         className='rounded-full w-full object-cover' />
       </Link>
-
     </SwiperSlide>
   ))}
-
 </Swiper>
 
 
@@ -98,8 +120,6 @@ className="mt-4">
  </div>
 )
 
-
 }
-
 
 export default TopPlay;
--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,31 @@
-const SongDetails = () => <div>SongDetails</div>;
+import {useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components'
+
+import { setActiveSong, playPause } from '../redux/features/playerSlice'
+import { useGetSongDetails, useGetTopChartsQuery } from '../redux/services/shazamCore'
+
+const SongDetails = () => {
+    const dispatch = useDispatch()
+    const { songid } = useParams()
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+
+    console.log(songid)
+
+    return (
+        <div className='flex flex-col'>
+            {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
+        <div className='mb-10'>
+            <h2 className='text-white text-3xl font-bold'>Lyrics:</h2>
+
+            <div className='mt-5'>
+
+                     
+            </div>
+        </div>
+        </div>
+    )
+}
 
 export default SongDetails;
