19d0376b804d263bae90934cfc6cd5fe398378a4
hariskurtishi <Hariskay95@gmail.com>
2022-11-03 13:43:17 +0100

    Added the search component

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,8 +3,19 @@ import { useNavigate } from "react-router-dom";
 import { FiSearch } from 'react-icons/fi'
 
 
-const Searchbar = () => (
-  <form autoComplete="off" 
+const Searchbar = () => {
+  const navigate = useNavigate()
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    navigate(`/search/${searchTerm}`)
+  }
+  
+  return (
+
+  <form onSubmit={handleSubmit} autoComplete="off" 
   className="p-2 text-gray-400 focus-within:tedxt-gray-600">
     <label htmlFor="search-field" className="sr-only">
       Search all songs
@@ -17,14 +28,14 @@ const Searchbar = () => (
   id='search-field'
   placeholder="Search"
   type="search"
-  value=""
-  onChange={() => {}}
+  value={searchTerm}
+  onChange={(e) => setSearchTerm(e.target.value)}
   className='flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4'
   
   />
       
     </div>
   </form>
-);
-
+)
+}
 export default Searchbar;
